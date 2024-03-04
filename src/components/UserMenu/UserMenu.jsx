dc61cda6d36d5967b79a75c93f3a55956d882395
KZapal <kamil.zapal@o2.pl>
2024-03-04 19:23:16 +0100

    change in layout

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,17 +1,22 @@
 import { useDispatch } from 'react-redux';
 import { logout } from '../../redux/auth/operations';
+import { useAuth } from 'hooks/useAuth';
+import css from './UserMenu.module.css';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
+  const { user } = useAuth();
 
   const handleLogout = () => {
     dispatch(logout());
   };
 
   return (
-    <div>
-      <p>pozniej@pozniej</p>
-      <button onClick={handleLogout}>Logout</button>
+    <div className={css.userMenu}>
+      <p>{user.name}</p>
+      <button className={css.btn} onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 };
